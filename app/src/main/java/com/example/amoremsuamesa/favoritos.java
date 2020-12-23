55597a00e7f101c95bc52fc7ae19b2537b8671e7
Vinicius <vinivius123@gmail.com>
2020-12-23 00:28:10 -0300

    GITKRAKEN É MENTIROSO

--- app/src/main/java/com/example/amoremsuamesa/favoritos.java
+++ app/src/main/java/com/example/amoremsuamesa/favoritos.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -21,7 +22,7 @@ public class favoritos extends AppCompatActivity {
 
         LayoutInflater inflater = LayoutInflater.from(this);
     //ESSA PARTE É SOMENTE PARA TESTAR OS PRODUTOS APARECENDO NA TELA
-        for (int i = 1; i == 50 || i < 50; i++) {
+        for (int i = 1; i == 5 || i < 5; i++) {
 
             View view = inflater.inflate(R.layout.cart_item_layout, imagem_produto, false);
 
@@ -29,7 +30,10 @@ public class favoritos extends AppCompatActivity {
             textView.setText("PRODUTO " + i);
 
             ImageView imageView = view.findViewById(R.id.cilImage);
-            imageView.setImageResource(R.drawable.amor_em_sua_mesa);
+            new DownloadImageTask((ImageView) imageView,(RelativeLayout)view.findViewById(R.id.loadingPanel) ).execute(staticStorageClass.carrinho.get(i).getImages().get(0));
+
+            //ImageView imageView = view.findViewById(R.id.cilImage);
+            //imageView.setImageResource(R.drawable.amor_em_sua_mesa);
 
             imageView.setOnClickListener(new View.OnClickListener() {
                 @Override
