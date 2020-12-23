cbdc33d0a2ca3a4955661cbfadf5e672525b172c
Vinicius <vinivius123@gmail.com>
2020-12-22 21:32:43 -0300

    ADIÇÃO DA TELA COMENTÁRIOS + BOTÃO AVALIAR

--- app/src/main/java/com/example/amoremsuamesa/teste.java
+++ app/src/main/java/com/example/amoremsuamesa/teste.java
@@ -1,42 +1,37 @@
 package com.example.amoremsuamesa;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.Button;
 import android.widget.RatingBar;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class teste extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //super.onCreate(savedInstanceState);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teste);
 
-       
+
+        TextView prod_nome = findViewById(R.id.prod_nome);
+        prod_nome.setText(staticStorageClass.selectedProduct.getNome());
+
+        TextView id_Preco = findViewById(R.id.id_Preco);
+        id_Preco.setText("R$ " + staticStorageClass.selectedProduct.getPreco());
+
+        TextView id_descricao = findViewById(R.id.id_descricao);
+        id_descricao.setText(staticStorageClass.selectedProduct.getDescricao());
+
+        //ImageView image_product = findViewById(R.id.imageButton4);
+        //image_product.setImageResource(staticStorageClass.selectedProduct.getImages());
 
 
-
-
-            TextView prod_nome = findViewById(R.id.prod_nome);
-            prod_nome.setText(staticStorageClass.selectedProduct.getNome());
-
-
-
-            //TextView id_Preco = findViewById(R.id.id_Preco);
-            //id_Preco.setText(staticStorageClass.carrinho.get(i).getPreco());
-
-
-
-
-
-
-         findViewById(R.id.prod_Btn).setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.prod_Btn).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 staticStorageClass.carrinho.add(staticStorageClass.selectedProduct);
@@ -45,14 +40,25 @@ public class teste extends AppCompatActivity {
         });
 
         RatingBar mBar = (RatingBar) findViewById(R.id.ratingBar);
-        findViewById(R.id.button8).setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.btn_comentar3).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 float nota = mBar.getRating();
-                Toast toast = Toast.makeText(v.getContext(),""+nota,Toast.LENGTH_SHORT);
+                Toast toast = Toast.makeText(v.getContext(), "" + nota, Toast.LENGTH_SHORT);
                 toast.show();
             }
         });
 
+        findViewById(R.id.btn_comentar).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivity(new Intent(teste.this, comentarios.class));
+            }
+        });
+
+
     }
 }
+
+
